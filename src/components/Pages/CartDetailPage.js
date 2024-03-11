6e5d7b5c452092002b7d4bf8631a096f60ef0ada
Arslan Ahmad <106490352+arslandeveloper1212@users.noreply.github.com>
2024-03-12 00:09:32 +0500

    working

--- src/components/Pages/CartDetailPage.js
+++ src/components/Pages/CartDetailPage.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import ReactImageMagnify from 'react-image-magnify';
 
 const CartDetailPage = () => {
-    
     const { id } = useParams();
-    // Convert id to integer
     const productId = parseInt(id);
-
     const products = useSelector(state => state.allcart.products);
-    console.log(products);
+    const [currentImage, setCurrentImage] = useState(null); // State to track current large image
 
-    // Find the product with the matching id
     const productIndex = products.findIndex(product => product.id === productId);
     const product = products[productIndex];
 
-    // console.log(product);
+    const handleThumbnailClick = (image) => {
+        setCurrentImage(image);
+    };
 
     return (
         <div>
@@ -23,19 +22,41 @@ const CartDetailPage = () => {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-lg-7 col-md-6 col-12'>
-                            <img src={product.image} alt={product.title} className='w-100' />
+                            <ReactImageMagnify {...{
+                                smallImage: {
+                                    alt: product.title,
+                                    isFluidWidth: true,
+                                    src: currentImage || product.image // Use currentImage if set, otherwise use main product image
+                                },
+                                largeImage: {
+                                    src: currentImage || product.image, // Use currentImage if set, otherwise use main product image
+                                    width: 1200,
+                                    height: 1800
+                                }
+                            }} />
+
+                            <div>
+                                {product.imagehalf.map((image, index) => (
+                                    <img
+                                        key={index}
+                                        src={image}
+                                        alt={`Image ${index + 1}`}
+                                        style={{ marginRight: '10px', marginBottom: '10px', width: '100px', height: 'auto', cursor: 'pointer' }}
+                                        onClick={() => handleThumbnailClick(image)} // Click handler to set current image
+                                    />
+                                ))}
+                            </div>
                         </div>
                         <div className='col-lg-5 col-md-6 col-12'>
-                            <h4 style={{marginTop:"60px"}}>{product.title}</h4>
-                            <p style={{marginTop:"60px"}}>{product.description}</p>
-                            <strong style={{fontSize:"23px", marginTop:"20px"}}>${product.price}</strong>
-                           
-                            <p style={{ marginTop:"20px"}}>Rating:{product.rating}</p>
+                            <h4 style={{ marginTop: "60px" }}>{product.title}</h4>
+                            <p style={{ marginTop: "20px" }}>{product.description}</p>
+                            <strong style={{ fontSize: "23px", marginTop: "20px" }}>${product.price}</strong>
+                            <p style={{ marginTop: "20px" }}>Rating: {product.rating}</p>
                             <div className='p-addtocart'>
-                            <Link to={`/cartdatapage`}>
-                            <button>Go to Cart</button>
-                            </Link>
-                        </div>
+                                <Link to={`/cartdatapage`}>
+                                    <button>Shopping Cart</button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,8 +67,6 @@ const CartDetailPage = () => {
             )}
         </div>
     );
-
-
 };
 
-            export default CartDetailPage;
+export default CartDetailPage;
