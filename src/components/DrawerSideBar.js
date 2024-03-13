882874b592c2ee9bfddffaca7bffa0301b4ea535
Arslan Ahmad <106490352+arslandeveloper1212@users.noreply.github.com>
2024-03-14 00:19:56 +0500

    working

--- src/components/DrawerSideBar.js
+++ src/components/DrawerSideBar.js
@@ -1,64 +1,83 @@
+import React from 'react';
+import { Menu, Drawer, Space, Badge } from 'antd';
+import { Link } from 'react-router-dom';
 import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
 import ToggleButton from '@mui/material/ToggleButton';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
+import { useNavigate } from 'react-router-dom';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useSelector } from 'react-redux';
+const { SubMenu } = Menu;
 
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import * as React from 'react';
+function MegaMenu() {
+    const [drawerVisible, setDrawerVisible] = React.useState(false);
 
-
-
-export default function DrawerSideBar() {
-    const [state, setState] = React.useState({
-      right: false,
-    });
-  
-    const toggleDrawer = (anchor, open) => () => {
-      setState({ ...state, [anchor]: open });
+    const toggleDrawer = () => {
+        setDrawerVisible(!drawerVisible);
     };
-  
-    const list = (anchor) => (
-      <Box
-        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-        role="presentation"
-        onClick={toggleDrawer(anchor, false)}
-        onKeyDown={toggleDrawer(anchor, false)}
-      >
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    );
-  
+
+    const item = useSelector(state => state.allcart.cart)
+    console.log(item.length)
+
+    const Navigate = useNavigate();
+
+    const handleClick = (e) => {
+      Navigate("/cartdatapage")
+    }
     return (
-      <div>
-        <ToggleButton style={{ border: "none" }} value="right" aria-label="right aligned" onClick={toggleDrawer('right', true)}>
-          <FormatAlignRightIcon />
-        </ToggleButton>
-        <Drawer
-          anchor="right"
-          open={state['right']}
-          onClose={toggleDrawer('right', false)}
-        >
-          {list('right')}
-        </Drawer>
-      </div>
+        <div style={{ backgroundColor: "white" }}>
+           
+            <Drawer
+                title="Sub Category"
+                placement="right"
+                closable={false}
+                onClose={toggleDrawer}
+                visible={drawerVisible}
+                width={300}
+            >
+                <MegaList />
+                <div className='section_nav_wrapper_clicked mt-5'>
+                    <PersonOutlineIcon />
+                    <span className='mb-0' style={{ cursor: "pointer" }}>Account </span>
+                    &nbsp;
+                    <Badge badgeContent={item.length} color="primary" > 
+                        <AddShoppingCartIcon onClick={handleClick} style={{cursor:"pointer"}} />
+                    </Badge>
+    </div>
+            </Drawer>
+            <ToggleButton 
+                style={{ border: "none" }} 
+                value="right" 
+                aria-label="right aligned" 
+                onClick={toggleDrawer}
+            >
+                <FormatAlignRightIcon />
+            </ToggleButton>
+        </div>
     );
-  }
+}
+
+function MegaList() {
+    return (
+        <Space direction='vertical'>
+            <Menu>
+                <Menu.Item key="Furniture">
+                    <Link to="/furniture">Furniture</Link>
+                </Menu.Item>
+                <Menu.Item key="Laptop">
+                    <Link to="/laptop">Laptop</Link>
+                </Menu.Item>
+            </Menu>
+            <Menu>
+                <Menu.Item key="HandBag">
+                    <Link to="/hand-bag">Hand Bag</Link>
+                </Menu.Item>
+                <Menu.Item key="Headphone">
+                    <Link to="/headphone">Headphone</Link>
+                </Menu.Item>
+            </Menu>
+        </Space>
+    );
+}
+
+export default MegaMenu;
